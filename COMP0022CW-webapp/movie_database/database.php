7f67a01b81172d69d5573338212a44712246406b
JUNZHANG LI <91993785+JUNZHANG-Li@users.noreply.github.com>
2024-03-01 17:31:22 +0000

    Delete COMP0022CW-webapp directory

--- COMP0022CW-webapp/movie_database/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $connection = mysqli_connect("db", "movieadmin", "secretpassword", "movie_database");
-
-  if (mysqli_connect_errno())
-    echo 'Failed to connect to the MySQL server: '. mysqli_connect_error();
-?>
